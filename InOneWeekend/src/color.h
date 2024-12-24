048d422f613de7250158c846d6cc0f740067afa5
ppprisha <prisha.sujin@gmail.com>
2024-12-24 17:39:54 -0500

    updated until sphere colored with normal vectors

---
+++ InOneWeekend/src/color.h
@@ -0,0 +1,37 @@
+#ifndef COLOR_H
+#define COLOR_H
+//==============================================================================================
+// Originally written in 2020 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
+
+#include <iostream>
+#include "interval.h"
+#include "vec3.h"
+
+using color = vec3;
+
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+ 
+    // Translate the [0,1] component values to the byte range [0,255].
+   // static const interval intensity(0.000, 0.999);
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+
+#endif
